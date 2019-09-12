0b547609220e94bce4e09100f326935715e3a6cb
Nerijus Tamošauskas <n.tamosauskas@spectrofinance.lt>
2019-09-12 12:16:34 +0300

    Close buffered reader

--- src/main/java/lt/spectrofinance/spectrocoin/merchantclient/SCMerchantClient.java
+++ src/main/java/lt/spectrofinance/spectrocoin/merchantclient/SCMerchantClient.java
@@ -127,12 +127,12 @@ public class SCMerchantClient {
 
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
-		StringBuffer result = new StringBuffer();
+		StringBuilder result = new StringBuilder();
 		String line = "";
 		while ((line = rd.readLine()) != null) {
 			result.append(line);
 		}
-
+		rd.close();
 		return convertToResponseObject(result.toString());
 	}
 
